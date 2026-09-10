cd2bba465685a6c38d7ace83a1579c68ba616e12
reda123454321 <elbahireda162@gmail.com>
2026-09-10 09:42:18 +0100

    Add files via upload

---
+++ prompt-sync.js
@@ -0,0 +1,12 @@
+const prompt = require("prompt-sync")();
+let name = prompt("what is your name?");
+let age = prompt("how old are you?");
+let course = prompt("where are you study?")
+
+
+console.log("NAME :" , name);
+console.log("AGE :" , age);
+console.log("COURSE :" , course);
+
+//npm install promt-sync
+//node test.js
