d6b29dc1d5baeed2621011f4a12b8d0ae702b499
reda123454321 <elbahireda162@gmail.com>
2026-09-10 10:10:55 +0100

    Add files via upload

---
+++ gere_unarticle.js
@@ -0,0 +1,10 @@
+const prompt = require("prompt-sync")();
+let order_name = prompt("please, enter order name : ")
+let price = prompt("how mush is it ?")
+let quantite = prompt("how many order do you buy ?")
+
+let total = price*quantite
+
+console.log("ORDER NAME :" , order_name)
+console.log("PRICE : " , price)
+console.log("QUANTITE : " , quantite)
