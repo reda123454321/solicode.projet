a9503234357ff54a3398d81bd624e900a8e3339a
reda123454321 <elbahireda162@gmail.com>
2026-09-10 15:20:52 +0100

    Update prompt-sync.js

--- prompt-sync.js
+++ prompt-sync.js
@@ -1,12 +1,16 @@
-const prompt = require("prompt-sync")();
-let name = prompt("what is your name?");
-let age = prompt("how old are you?");
-let course = prompt("where are you study?")
+const prompt = require("prompt-sync")()
 
 
-console.log("NAME :" , name);
-console.log("AGE :" , age);
-console.log("COURSE :" , course);
+let name = prompt("what is your name?")
+let last_name = prompt("what is your last name?")
+let age = prompt("how old are you?")
+let school = prompt("school ?")
 
-//npm install promt-sync
-//node test.js
+console.log("NAME : ", name)
+console.log("LAST NAME : ", last_name)
+console.log("AGE : ", age)
+console.log("SCHOOL : ", school)
+
+console.log("THANK YOU ",name, "!")
+//npm install prompt-sync
+//node prompt-sync.js
