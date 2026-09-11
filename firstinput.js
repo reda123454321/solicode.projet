dfcb3a5b9020533b85ddd3fddea46620291cb68e
reda123454321 <elbahireda162@gmail.com>
2026-09-11 16:05:44 +0100

    Add files via upload

---
+++ firstinput.js
@@ -0,0 +1,19 @@
+const input = require("prompt-sync")();
+
+note =  input("how mush your mathematique note ?")
+
+if(note < 10){
+    console.log("you are stupid")
+
+}else if(note >14){
+    console.log("you are perfect")
+}else{
+    console.log("you are good")
+}
+
+
+
+
+
+
+
